d6b54ddd65e9a73a4894ce186d8a279b38bddc82
Adam Sherwood <theherk@gmail.com>
2022-07-15 15:05:13 +0200

    Add additional domain and mapping for alternates.

--- main.tf
+++ main.tf
@@ -152,9 +152,9 @@ resource "aws_api_gateway_deployment" "this" {
 }
 
 resource "aws_api_gateway_domain_name" "this" {
-  count = var.domain_name != null ? 1 : 0
+  for_each = toset(concat(try([coalesce(var.domain_name)], []), var.domain_names_alternate))
 
-  domain_name              = var.domain_name
+  domain_name              = each.key
   regional_certificate_arn = var.certificate_arn
 
   endpoint_configuration {
@@ -162,24 +162,24 @@ resource "aws_api_gateway_domain_name" "this" {
   }
 }
 
+resource "aws_api_gateway_base_path_mapping" "this" {
+  for_each = toset(concat(try([coalesce(var.domain_name)], []), var.domain_names_alternate))
+
+  api_id      = aws_api_gateway_rest_api.this.id
+  stage_name  = aws_api_gateway_stage.this.stage_name
+  domain_name = aws_api_gateway_domain_name.this[each.key].domain_name
+}
+
 resource "aws_route53_record" "this" {
   count = var.domain_name != null ? 1 : 0
 
-  name    = aws_api_gateway_domain_name.this[0].domain_name
+  name    = aws_api_gateway_domain_name.this[var.domain_name].domain_name
   type    = "A"
   zone_id = var.zone_id
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.this[0].regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.this[0].regional_zone_id
+    name                   = aws_api_gateway_domain_name.this[var.domain_name].regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.this[var.domain_name].regional_zone_id
   }
-}
-
-resource "aws_api_gateway_base_path_mapping" "this" {
-  count = var.domain_name != null ? 1 : 0
-
-  api_id      = aws_api_gateway_rest_api.this.id
-  stage_name  = aws_api_gateway_stage.this.stage_name
-  domain_name = aws_api_gateway_domain_name.this[0].domain_name
 }
