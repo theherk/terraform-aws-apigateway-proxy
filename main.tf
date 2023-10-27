0694183e8d19c647e59873f96bfbe29081333286
Adam Sherwood <theherk@gmail.com>
2023-10-27 08:52:26 +0200

    Include authorizer in triggers for deployment.

--- main.tf
+++ main.tf
@@ -158,7 +158,8 @@ resource "aws_api_gateway_deployment" "this" {
       aws_api_gateway_method.this,
       aws_api_gateway_integration.this,
       aws_api_gateway_integration_response.this,
-      aws_api_gateway_method_response.this
+      aws_api_gateway_method_response.this,
+      var.authorizer
     ]))
   }
 
